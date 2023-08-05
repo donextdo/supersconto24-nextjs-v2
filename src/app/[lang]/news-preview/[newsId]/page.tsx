ba91a0b0de8611775c1c311337308b015b3e8a52
kalanaw2 <manjitharock@gmail.com>
2023-08-05 10:13:28 +0530

    social share

--- src/app/[lang]/news-preview/[newsId]/page.tsx
+++ src/app/[lang]/news-preview/[newsId]/page.tsx
@@ -1,8 +1,7 @@
-'use client'
-import { useEffect, useState } from "react";
+
+
 import baseUrl from "../../../../../utils/baseUrl";
-import axios from "axios";
-import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
+// import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
 import siteUrl from "../../../../../utils/siteUrl";
 
 interface IProps {
@@ -20,29 +19,39 @@ interface newsInfo {
     image: string;
 }
 
-const NewsPage = ({ params }: IProps) => {
-    const newsId = params.newsId;
-    const [news, setNews] = useState<newsInfo>({
-        status: '',
-        expiredDate: '',
-        _id: '',
-        title: '',
-        description: 'string',
-        image: ''
-    })
-    useEffect(() => {
-        fetchData()
-    }, []);
+async function getNews(newsId:any) {
+    const response = await fetch(`${baseUrl}/news/find/${newsId}`,{ cache: 'no-store' })
+    const data =await response.json()
+    return data
+}
 
-    async function fetchData() {
-        try {
-            const res = await axios.get(`${baseUrl}/news/find/${newsId}`);
-            console.log(res.data)
-            setNews(res.data)
-        } catch (err) {
-            console.log(err);
-        }
-    }
+const NewsPage =  async ({ params }: IProps) => {
+    const newsId = params.newsId;
+    // const news = await fetch(`${baseUrl}/news/find/${newsId}`,{ cache: 'no-store' }).then((res) => res.json())
+    const news = await getNews(newsId)
+    // const [news, setNews] = useState<newsInfo>({
+    //     status: '',
+    //     expiredDate: '',
+    //     _id: '',
+    //     title: '',
+    //     description: 'string',
+    //     image: ''
+    // })
+
+
+    // useEffect(() => {
+    //     fetchData()
+    // }, []);
+
+    // async function fetchData() {
+    //     try {
+    //         const res = await axios.get(`${baseUrl}/news/find/${newsId}`);
+    //         console.log(res.data)
+    //         setNews(res.data)
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // }
 
     const encodedUrl = encodeURIComponent(`${siteUrl}/news-preview/${newsId}`);
     const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
@@ -57,6 +66,8 @@ const NewsPage = ({ params }: IProps) => {
         day: "numeric",
     });
 
+    console.log(news)
+
     return (
         <div className="container mx-auto xl:px-40 px-5 py-6 space-y-4">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
@@ -64,7 +75,7 @@ const NewsPage = ({ params }: IProps) => {
                     <h6 className="text-lg font-semibold text-left ">{news.title}</h6>
                     <h6 className="text-xs">Expire Date - {formattedDate}</h6>
                     <div className="flex gap-2">
-                        <FacebookShareButton url={facebookShareUrl}>
+                        {/* <FacebookShareButton url={facebookShareUrl}>
                             <FacebookIcon size={35} round={true} />
                         </FacebookShareButton>
                         <TwitterShareButton url={twitterShareUrl}>
@@ -72,7 +83,7 @@ const NewsPage = ({ params }: IProps) => {
                         </TwitterShareButton>
                         <WhatsappShareButton url={whatsappShareUrl}>
                             <WhatsappIcon size={35} round={true} />
-                        </WhatsappShareButton>
+                        </WhatsappShareButton> */}
                     </div>
                 </div>
                 <div>
