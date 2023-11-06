4728851ebd24356ae0b31a4e28ef4d2fca0a3cd8
Githendra McShane <githendramax@gmail.com>
2023-11-07 00:18:51 +0530

    remove red

--- src/app/[lang]/features/product/ProductPopup.tsx
+++ src/app/[lang]/features/product/ProductPopup.tsx
@@ -376,7 +376,7 @@ const ProductPopup = ({ setProductPopup, proId }: any) => {
 
                                         {data.discount && (
                                             <span className="my-1 text-red-700 text-lg font-semibold">
-                        red                        {getPrice(newPrice)}
+                                                {getPrice(newPrice)}
                                             </span>
                                         )}
                                     </div>
